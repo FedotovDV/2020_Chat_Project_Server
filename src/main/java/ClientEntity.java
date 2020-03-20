3d32979fca2a12ecd6b15b86b61667126c9d8832
Dmitriy <fed3375546@yandex.ru>
2020-03-21 00:46:42 +0300

    refactoring Server

---
+++ src/main/java/ClientEntity.java
@@ -0,0 +1,54 @@
+import com.google.gson.Gson;
+import lombok.SneakyThrows;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+
+public class ClientEntity implements Runnable, Observer {
+
+    private Client client;
+    private Socket socket;
+    private MyServer server;
+
+    public ClientEntity(Socket socket, MyServer server) {
+        this.socket = socket;
+        this.server = server;
+    }
+
+    @SneakyThrows
+    @Override
+    public void run() {
+        BufferedReader clientReader = new BufferedReader(new InputStreamReader(
+                socket.getInputStream()));
+        while (true) {
+            String clientMessage = clientReader.readLine();
+            if (clientMessage.startsWith("REGISTRATION")) {
+                String[] logPass = clientMessage.substring(12).split(":");
+                client = new Client(logPass[0], logPass[1].toCharArray());
+                System.out.println("New client connected: " +
+                        logPass[0] + " " + logPass[1]);
+                server.addObserver(this);
+                server.notifyObservers(client.getUserName() + ": " + clientMessage);
+            } else {
+                System.out.println(clientMessage);
+                server.notifyObservers(client.getUserName() + ": " + clientMessage);
+            }
+        }
+    }
+    @SneakyThrows
+    @Override
+    public void notifyObservers(String message) {
+        BufferedWriter writer = new BufferedWriter( new PrintWriter(socket.getOutputStream()));
+        writer.write(message);
+        writer.newLine();
+        writer.flush();
+
+    }
+
+//    public boolean registration(){
+//
+//    }
+}
--- src/main/java/MyServer.java
+++ src/main/java/MyServer.java
@@ -1,27 +1,66 @@
 
+import com.google.gson.Gson;
+
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.List;
 
-public class MyServer {
+public class MyServer  implements Observable{
 
     public final static int PORT = 8290;
+    private volatile  List<Observer> clients = new ArrayList<>();
 
     public void start() {
-
-        System.out.println("====Server starts====");
+        System.out.println("==START SERVER==");
         try {
             ServerSocket serverSocket = new ServerSocket(PORT);
+            Socket socket = null;
             while (true) {
-                Socket socket = serverSocket.accept();
-                if (socket != null) {
-                    ObjectInputStream clientReader = new ObjectInputStream(socket.getInputStream());
-                    Client user1 = (Client) clientReader.readObject();
-                    System.out.println(user1);
+                if (socket == null) {
+                    socket = serverSocket.accept();
+                } else {
+                    ClientEntity clientEntity = new ClientEntity(socket, this);
+                    new Thread(clientEntity).start();
+                    socket = null;
                 }
             }
-        } catch (IOException | ClassNotFoundException e) {
+        } catch (IOException e) {
             e.printStackTrace();
         }
     }
+
+    @Override
+    public void addObserver(Observer o) {
+        clients.add(o);
+    }
+
+    @Override
+    public void stopObserver(Observer o) {
+        /**/
+    }
+
+    @Override
+    public void notifyObservers(String message) {
+        for (Observer client : clients) {
+            client.notifyObservers(message);
+        }
+    }
 }
+
+
+//    if (socket != null) {
+////                    ObjectInputStream clientReader = new ObjectInputStream(socket.getInputStream());
+////                    Client user1 = (Client) clientReader.readObject();
+////                    System.out.println(user1);
+//
+//                    BufferedReader clientReader =
+//                            new BufferedReader(new InputStreamReader(socket.getInputStream()));
+////                    String clientMessage = clientReader.readLine();
+////                    System.out.println(clientMessage);
+//                    Gson gson = new Gson();
+//                    String json = clientReader.readLine();
+//                    Client client = gson.fromJson(json, Client.class);
+//                    System.out.println(client.toString());
+//                }
